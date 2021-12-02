4abd6556d22bd5b5b70c2f903bae022b53c178b7
Rachelle <rachelle.yeo@outlook.com>
2021-12-02 21:59:36 +0800

    fix: reset bills + friends to home
    restructured too early, finish proper function first before restructring

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,73 @@
-import Friends from "../../components/Friends";
-import Bills from "../../components/Bills";
+import { useState, useRef, useEffect } from "react";
+
+import BillsList from "../../components/Bills/List";
+import FriendsList from "../../components/Friends/List";
 
 import styled from "styled-components";
 
 function Home() {
+  const [bills, setBill] = useState([
+    { name: "Jollibee on Wednesday", amount: 85 },
+    { name: "Grab Fare", amount: 40 },
+  ]);
+  const billRef = useRef();
+  const billNameRef = useRef();
+
+  const [friends, setFriendsList] = useState([
+    { owner: "Rachelle", name: "Charles" },
+  ]);
+  const friendsRef = useRef();
+
+  const handleAddBill = (e) => {
+    const billName = billNameRef.current.value;
+    const bill = billRef.current.value;
+    if (bill === "") return null;
+    console.log(billName, bill);
+    setBill((prevList) => {
+      return [...prevList, { name: billName, amount: bill }];
+    });
+    billNameRef.current.value = null;
+    billRef.current.value = null;
+  };
+
+  const handleAddFriend = (e) => {
+    const name = friendsRef.current.value;
+    if (name === "") return null;
+    setFriendsList((prevList) => {
+      return [...prevList, { owner: "Rachelle", name: name }];
+    });
+    friendsRef.current.value = null;
+  };
+
+  const totalBills = bills.reduce((totalBillsAmount, bill) => {
+    console.log(`Total: ${totalBillsAmount}`);
+    console.log(`Item: ${bill.amount}`);
+    return totalBillsAmount + bill.amount;
+  }, 0);
+
+  // useEffect(() => {
+  //   const storedFriendsList = JSON.parse(localStorage.getItem("Friends"));
+  //   if (storedFriendsList) setFriendsList(storedFriendsList);
+  // }, []);
+
+  // useEffect(() => {
+  //   localStorage.setItem("Friends", JSON.stringify(friends));
+  // }, [friends]);
   return (
     <ContentWrapper>
-      <Bills />
-      <Friends />
+      <h1>This is home page yay</h1>
+      <BillWrapper>
+        <BillsList bills={bills} />
+        <input ref={billNameRef} type="text" placeholder="Name of Bill" />
+        <input ref={billRef} type="number" placeholder="Amount" />
+        <button onClick={handleAddBill}>Add Bill</button>
+        Total Settlement Amount: {totalBills}
+      </BillWrapper>
+      <FriendsWrapper>
+        <FriendsList friends={friends} />
+        <input ref={friendsRef} type="text" />
+        <button onClick={handleAddFriend}>Add Friend</button>
+      </FriendsWrapper>
     </ContentWrapper>
   );
 }
@@ -17,4 +77,16 @@ export default Home;
 const ContentWrapper = styled.section`
   display: inline-flex;
   justify-content: space-between;
+`;
+
+const BillWrapper = styled.div`
+  padding: 20px;
+  box-shadow: -6px -8px 0 0 darkolivegreen;
+`;
+
+const FriendsWrapper = styled.div`
+  width: 40%;
+  max-width: 300px;
+  padding: 20px;
+  box-shadow: 6px 8px hotpink;
 `;
